50d9e070686101aaae738cc03794e044c2186ca9
lixiaodong <lixiaodong@souyidai.com>
2016-03-17 09:45:59 +0800

    'm'

--- src/test/java/com/blog/test/thread/JvmLoadTest.java
+++ src/test/java/com/blog/test/thread/JvmLoadTest.java
@@ -3,5 +3,45 @@ package com.blog.test.thread;
 /**
  * Created by xiaodong on 2016/3/17.
  */
-public class JvmLoadTest {
+class SSClass
+{
+    static
+    {
+        System.out.println("SSClass");
+    }
+}
+class SuperClass extends SSClass
+{
+    static
+    {
+        System.out.println("SuperClass init!");
+    }
+
+    public static int value = 123;
+
+    public SuperClass()
+    {
+        System.out.println("init SuperClass");
+    }
+}
+class SubClass extends SuperClass
+{
+    static
+    {
+        System.out.println("SubClass init");
+    }
+
+    static int value=2;
+
+    public SubClass()
+    {
+        System.out.println("init SubClass");
+    }
+}
+class NotInitialization
+{
+    public static void main(String[] args)
+    {
+        System.out.println(SubClass.value);
+    }
 }
